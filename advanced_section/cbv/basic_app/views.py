9117442bb3a68ab6bcecf1e318f716ddce443e50
Hyung Tae C. Figur <hyu_03@hotmail.com>
2020-03-15 23:29:09 -0300

    Started Advanced Section

---
+++ advanced_section/cbv/basic_app/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.views.generic import View, TemplateView, ListView, DetailView
+# from django.http import HttpResponse
+from . import models
+
+# Create your views here.
+# def index(request):
+#     return render(request, 'index.html')
+
+# class CBView(View):
+#     def get(self, request):
+#         return HttpResponse("CLASS BASED VIEWS ARE COOL!")
+
+class IndexView(TemplateView):
+    template_name= 'index.html'
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = 'BASIC INJECTION!'
+        return context
+
+class SchoolListView(ListView):
+    context_object_name = 'schools'
+    model = models.School # default object name is school_list
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
+    model = models.School # default object name is school
+    template_name = 'basic_app/school_detail.html'
