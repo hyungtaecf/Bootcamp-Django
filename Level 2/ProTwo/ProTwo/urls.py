12ce507016445265fff6855dccf97fe00b54db17
Hyung Tae C. Figur <hyu_03@hotmail.com>
2020-03-19 11:22:38 -0300

    Debug Toolbar

--- Level 2/ProTwo/ProTwo/urls.py
+++ Level 2/ProTwo/ProTwo/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from django.conf import settings
 from AppTwo import views
 
 urlpatterns = [
@@ -25,3 +26,9 @@ urlpatterns = [
     url(r'^AppTwo/',include('AppTwo.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/',include(debug_toolbar.urls))
+    ] + urlpatterns
